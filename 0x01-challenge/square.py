72c49a315aa4d0f14c41083f99c2de8415e4da48
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-14 14:25:13 +0300

    fixing square code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""
+square class module
+"""
+
 
 class square():
-    
+    """Describes the square"""
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """initializing the square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,12 +20,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string representation of the square"""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
+    """create an object instance of a square class"""
 
     s = square(width=12, height=9)
     print(s)
