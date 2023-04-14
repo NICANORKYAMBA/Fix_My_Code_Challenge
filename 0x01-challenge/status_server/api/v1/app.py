d29a88a9faa5b6c8b08bff595fca6676c8a8c2e5
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-04-14 12:45:23 +0300

    0. Server status

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -5,9 +5,6 @@ Web server
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
-
-print("Initializing app...")  # added print statement
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -5,8 +5,6 @@ from flask import jsonify
 
 from api.v1.views import app_views
 
-print("Initializing app_views blueprint...")  # added print statement
-
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
